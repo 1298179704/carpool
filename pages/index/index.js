12beb0025fddb0c4a49a67116923a30c5a4a17df
Ran <1298179704@qq.com>
2018-10-15 01:14:29 +0800

    20181015

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,30 +4,12 @@ const app = getApp()
 
 Page({
   data :{
-    lonitude:"",
-    latitude:""
 
-  },
-  onShow(){
-    this.getLocation();
-  },
-  getLocation(){
-    wx.getLocation({
-      type: 'gcj02',
-      success: this.handleGetLocationSucc.bind(this)
-    })
-    
-  },
-  handleGetLocationSucc(res){
-    this.setData({
-      longitude:res.longitude,
-      latitude:res.latitude
-    })
-  },
   onShareAppMessage(){
     title:'深圳技术大学内部拼车小程序' 
     path:'/pages/index/index'
  
-  }  
+  } 
+  } 
 
 })
